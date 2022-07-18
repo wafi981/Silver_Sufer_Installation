5cb4f1490ca892bae1567cf1ddae7c21404fe253
wafi981 <wafihasan567@gmail.com>
2022-07-18 16:32:38 +0530

    first commit

---
+++ Start.sh
@@ -0,0 +1,17 @@
+
+git clone https://github.com/ggreer/the_silver_searcher.git
+
+cd the_silver_searcher
+
+apt-get install silversearcher-ag
+
+
+apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
+
+
+./build.sh
+
+
+sudo make install
+
+
